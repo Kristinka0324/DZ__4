372a934a5a278f199ed7b89d95d526752f2ad640
Kristina <kristinka.antipova94@gmail.com>
2021-11-12 19:59:48 +0500

    DZ_4

---
+++ DZ__4_(1)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DZ__4__1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetFullName("Назарбаев", "Нурсултан", "Абишевич");
+            GetFullName("Путин", "Владимир", "Владимирович");
+            GetFullName("Антипова", "Кристина", "Васильевна");
+        }
+        static void GetFullName(string lastName, string firstName, string patronymic)
+        {
+            Console.WriteLine($"{lastName} {firstName} {patronymic}");
+        }
+
+    }
+}
---
+++ DZ__4_(2)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace DZ__4__2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Console.ReadLine().Split().Select(int.Parse).Sum());
+            Console.ReadKey();
+        }
+
+    }
+}
